af256619aff9c041ceabe549040c575a8fc252c0
Abarnasuresh <88279523+Abarnasuresh@users.noreply.github.com>
2022-01-02 12:14:31 +0530

    Create rest.js

---
+++ CHAPTER 9/rest.js
@@ -0,0 +1,14 @@
+function print1(...args){
+    console.log(args);
+    
+  }
+  print1(...[1,2,3],4,5)
+
+/*function print(a,b,c, ...args)
+{
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(args);
+}
+print(...[1,2,3],4,5);*/
