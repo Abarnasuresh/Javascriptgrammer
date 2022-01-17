015e1aa5b191d62ebd53e459aed5b8e1ad880120
Abarnasuresh <88279523+Abarnasuresh@users.noreply.github.com>
2022-01-17 21:14:36 +0530

    Delete Q.1 prime.js

--- EVALUATION 2/Q.1 prime.js
+++
@@ -1,9 +0,0 @@
-function primeNumber(n) {
-    let arr = [2];
-    for (let i = 3; i <= n; i += 2)
-        if (!((i % 3 == 0 && i > 3) || (i % 5 == 0 && i > 5) || (i % 7 == 0 && i > 7) || (i % 9 == 0 && i > 9))) {
-            arr.push(i);
-        }
-    return arr;
-}
-console.log(primeNumber(200));
