ea042e0fa642b65b7e89322b66523a9ac7d49436
Abarnasuresh <88279523+Abarnasuresh@users.noreply.github.com>
2022-01-18 20:44:14 +0530

    Create Q.16 Differences between named function and assigning functions.js

---
+++ EVALUATION 2/Q.16 Differences between named function and assigning functions.js
@@ -0,0 +1,13 @@
+//named function
+function cricketAcc(bat,helmet) 
+{ 
+  return bat+helmet 
+} 
+let k=cricketAcc("honda bat ","suprme helmet"); 
+console.log(k)
+//Assigning function
+let s=function(a,b)
+{    
+  return a*b;   
+}  
+console.log(s(4,5));
